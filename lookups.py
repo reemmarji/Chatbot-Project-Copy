2914261537914efc8659365a34a4d0bcd3bf035b
REEM-LAPTOP\user <reem.marji2112@gmail.com>
2023-07-10 19:14:19 +0300

    all changes

--- lookups.py
+++ lookups.py
@@ -3,5 +3,7 @@ from enum import Enum
 
 class LLM_MODEL_TYPE(Enum):
     DAVINCI = "text-davinci-003"
+    FINE_TUNED_MODEL = "davinci:ft-personal-2023-06-21-14-33-02"
+    GPT = "gpt-3.5-turbo"
 
 
