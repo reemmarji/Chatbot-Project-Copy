0b5dfb780ae7ddfe323ae66d067e4f9bb874e0bb
Ronan <ronan.tanios@epfl.ch>
2023-07-11 17:00:26 +0300

    add requir

--- ChatLAU.py
+++ ChatLAU.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.vectorstores import Pinecone
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
